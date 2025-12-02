79866052d55580c7c583d9ab15d7f51cb7c911c6
pollybatchelor <pollybatchelor97@gmail.com>
2025-12-03 09:33:43 +1300

    Fix TypeScript error

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// @ts-ignore
 import App from './App.jsx'
 
+import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
